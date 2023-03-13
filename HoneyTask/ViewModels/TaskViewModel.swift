382ebf8a82bd85651a09bd75e70a9c6b83e69494
Gökberk Kulaç <gokberkkulac@gmail.com>
2023-03-13 16:41:31 +0300

    Changed So many different UIView There will be more and we need to more utilities and functionalities to this app for later section

--- HoneyTask/ViewModels/TaskViewModel.swift
+++ HoneyTask/ViewModels/TaskViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreData
 
 class TaskViewModel: ObservableObject {
     @Published var currentSection :String = "Today"
@@ -16,4 +17,19 @@ class TaskViewModel: ObservableObject {
     @Published var taskDeadline : Date = Date()
     @Published var taskType : String = "Casual"
     @Published var taskDescription : String = ""
+    
+    func addTask(context:NSManagedObjectContext){
+         let task = Task(context: context)
+        task.title = taskTitle
+        task.color = taskColor
+        task.deadline = taskDeadline
+        task.type = taskType
+        task.taskDescription = taskDescription
+        
+        do {
+            try context.save()
+        } catch {
+             
+        }
+    }
 }
